74aa837e73cf5c44c1b2aa841d3d2f02beb82c5f
David Pérez-Suárez <dps.helio@gmail.com>
2018-09-14 11:50:06 +0100

    Include a regression test
    This is the example we've been told that it works

---
+++ fields_overlap/test_overlap.py
@@ -0,0 +1,7 @@
+from overlap import oa
+
+def test_basic():
+    ''' Tests that basic example works '''
+    big_field = (1, 1, 4, 4)
+    inner_field = (2, 2, 3, 3)
+    assert oa(big_field, inner_field) == 1
