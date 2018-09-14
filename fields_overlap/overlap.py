e34d2ec9fa0b1ca34dc8fe30d39bc94bde5c2bc1
David Pérez-Suárez <dps.helio@gmail.com>
2018-09-14 14:19:43 +0100

    meaningful variable names

--- fields_overlap/overlap.py
+++ fields_overlap/overlap.py
@@ -2,31 +2,32 @@ import matplotlib.pyplot as plt
 from matplotlib.path import Path
 import matplotlib.patches as patches
 
-def sf(f1, f2):
-    def vertices(L,B,R,T):
-        verts = [(L,B),(L,T),(R,T),(R,B),(L,B)]
+def show_fields(field1, field2):
+    def vertices(left, bottom, right, top):
+        verts = [(left, bottom), (left, top), (right, top), (right, bottom), (left, bottom)]
         return verts
+
     codes = [Path.MOVETO, Path.LINETO, Path.LINETO, Path.LINETO, Path.CLOSEPOLY]
-    p1 = Path(vertices(*f1), codes)
-    p2 = Path(vertices(*f2), codes)
+    path1 = Path(vertices(*field1), codes)
+    path2 = Path(vertices(*field2), codes)
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    pa1 = patches.PathPatch(p1, facecolor='orange', lw=2)
-    pa2 = patches.PathPatch(p2, facecolor='blue', lw=2)
-    ax.add_patch(pa1)
-    ax.add_patch(pa2)
+    patch1 = patches.PathPatch(path1, facecolor='orange', lw=2)
+    patch2 = patches.PathPatch(path2, facecolor='blue', lw=2)
+    ax.add_patch(patch1)
+    ax.add_patch(patch2)
     ax.set_xlim(0,5)
     ax.set_ylim(0,5)
     fig.show()
 
 
-def oa(f1, f2):
-    L1, B1, T1, R1 = f1
-    L2, B2, T2, R2 = f2
-    overL = max(L1, L2)
-    overB = max(B1, B2)
-    overR = min(R1, R2)
-    overT = min(T1, T2)
-    overH = (overT-overB)
-    overW = (overR-overL)
-    return overH*overW
+def overlap_area(field1, field2):
+    left1, bottom1, top1, right1 = field1
+    left2, bottom2, top2, right2 = field2
+    overlap_left = max(left1, left2)
+    overlap_bottom = max(bottom1, bottom2)
+    overlap_right = min(right1, right2)
+    overlap_top = min(top1, top2)
+    overlap_height = (overlap_top - overlap_bottom)
+    overlap_width = (overlap_right - overlap_left)
+    return overlap_height * overlap_width
