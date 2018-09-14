4627da87924644dadf4d3a1db18b6b93dc68b291
David Pérez-Suárez <dps.helio@gmail.com>
2018-09-14 14:20:06 +0100

    Update test to the new function name

--- fields_overlap/test_overlap.py
+++ fields_overlap/test_overlap.py
@@ -1,7 +1,7 @@
-from overlap import oa
+from overlap import overlap_area
 
 def test_basic():
     ''' Tests that basic example works '''
     big_field = (1, 1, 4, 4)
     inner_field = (2, 2, 3, 3)
-    assert oa(big_field, inner_field) == 1
+    assert overlap_area(big_field, inner_field) == 1
