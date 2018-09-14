d115866d79d26e268f3e407a63007aed4cd4b257
David Pérez-Suárez <dps.helio@gmail.com>
2018-09-14 18:17:48 +0100

    Add partial overlap test

--- fields_overlap/test_overlap.py
+++ fields_overlap/test_overlap.py
@@ -5,3 +5,10 @@ def test_basic():
     big_field = (1, 1, 4, 4)
     inner_field = (2, 2, 3, 3)
     assert overlap_area(big_field, inner_field) == 1
+
+
+def test_partial_overlap():
+    ''' Tests when there's a partial overlap'''
+    base_field = (1, 1, 4, 3)
+    over_field = (2, 2, 3, 4)
+    assert overlap_area(base_field, over_field) == 1
