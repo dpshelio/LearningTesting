2bd4595bf5855da1de19ba7fb0324ef4988a997f
David Pérez-Suárez <dps.helio@gmail.com>
2018-09-14 18:33:32 +0100

    Fix inconsistency between plotting and area calculation

--- fields_overlap/overlap.py
+++ fields_overlap/overlap.py
@@ -22,8 +22,8 @@ def show_fields(field1, field2):
 
 
 def overlap_area(field1, field2):
-    left1, bottom1, top1, right1 = field1
-    left2, bottom2, top2, right2 = field2
+    left1, bottom1, right1, top1 = field1
+    left2, bottom2, right2, top2 = field2
     overlap_left = max(left1, left2)
     overlap_bottom = max(bottom1, bottom2)
     overlap_right = min(right1, right2)
