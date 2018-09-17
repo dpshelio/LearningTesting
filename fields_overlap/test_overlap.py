59478af2f41b310c3a407537d0e2f834c0d48eb0
David Pérez-Suárez <dps.helio@gmail.com>
2018-09-17 23:35:03 +0100

    DRYing the tests

--- fields_overlap/test_overlap.py
+++ fields_overlap/test_overlap.py
@@ -1,68 +1,23 @@
+import pytest
 from pytest import approx, raises
 
 from overlap import overlap_area
 
-def test_basic():
+@pytest.mark.parametrize("big_field, inner_field, area", [
+    ((1, 1, 4, 4), (2, 2, 3, 3), 1),
+    ((1, 1, 4, 3), (2, 2, 3, 4), 1), # Tests when there's a partial overlap
+    ((1, 0, 3, 5), (2, 4, 4, 6), 1), # Test when there is a box in a corner
+    ((1, 1, 4, 4), (2, 2, 3, 4), 2), # Test when there is an edge
+    ((1, 1, 4, 4), (2, 1, 3, 4), 3), # Test when there is an edge
+    ((1, 1, 4, 4), (2, 4, 3, 5), 0), # Test when they are touching on the outside
+    ((0, 0, 3, 3), (4, 4, 5, 5), 0), # Test when they are not touching each other
+    ((1, 1., 3.5, 3.5), (3, 3, 5, 5), 0.5 * 0.5), # Test that still works when using floats
+    ((1, 1., 3.3, 3.1), (3, 3, 5, 5), approx(0.3 * 0.1, rel=1e-3)), # Test that still works when using floats
+    ((-1, -1, 1, 1), (0, -2, 1, 2), 2), # Tests that basic example works
+])
+def test_overlap_cases(big_field, inner_field, area):
     ''' Tests that basic example works '''
-    big_field = (1, 1, 4, 4)
-    inner_field = (2, 2, 3, 3)
-    assert overlap_area(big_field, inner_field) == 1
-
-
-def test_partial_overlap():
-    ''' Tests when there's a partial overlap'''
-    base_field = (1, 1, 4, 3)
-    over_field = (2, 2, 3, 4)
-    assert overlap_area(base_field, over_field) == 1
-
-
-def test_corner_overlap():
-    ''' Test when there is a box in a corner'''
-    base_field = (1, 0, 3, 5)
-    over_field = (2, 4, 4, 6)
-    assert overlap_area(base_field, over_field) == 1
-
-
-def test_edge_touching():
-    ''' Test when there is an edge '''
-    base_field = (1, 1, 4, 4)
-    over_field = (2, 2, 3, 4)
-    assert overlap_area(base_field, over_field) == 2
-
-
-def test_2opposite_edge_touching():
-    ''' Test when there is an edge '''
-    base_field = (1, 1, 4, 4)
-    over_field = (2, 1, 3, 4)
-    assert overlap_area(base_field, over_field) == 3
-
-
-def test_outside_edge_touching():
-    ''' Test when they are touching on the outside '''
-    base_field = (1, 1, 4, 4)
-    over_field = (2, 4, 3, 5)
-    assert overlap_area(base_field, over_field) == 0
-
-
-def test_no_overlap():
-    ''' Test when they are not touching each other '''
-    base_field = (0, 0, 3, 3)
-    over_field = (4, 4, 5, 5)
-    assert overlap_area(base_field, over_field) == 0
-
-
-def test_floats():
-    ''' Test that still works when using floats '''
-    base_field = (1, 1., 3.5, 3.5)
-    over_field = (3, 3, 5, 5)
-    assert overlap_area(base_field, over_field) == 0.5 * 0.5
-
-
-def test_floats_again():
-    ''' Test that still works when using floats '''
-    base_field = (1, 1., 3.3, 3.1)
-    over_field = (3, 3, 5, 5)
-    assert overlap_area(base_field, over_field) == approx(0.3 * 0.1, rel=1e-3)
+    assert overlap_area(big_field, inner_field) == area
 
 
 def test_negative_basic():
@@ -71,10 +26,3 @@ def test_negative_basic():
     inner_field = (-2, -2, -3, -3)
     with raises(ValueError, message=" Coordinates need to be entered (left, bottom, right, top) "):
         overlap_area(big_field, inner_field)
-
-
-def test_negative_basic2():
-    ''' Tests that basic example works '''
-    big_field = (-1, -1, 1, 1)
-    inner_field = (0, -2, 1, 2)
-    assert overlap_area(big_field, inner_field) == 2
